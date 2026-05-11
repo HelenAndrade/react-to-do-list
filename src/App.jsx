15aa08f5491f89aee230919b7fafba3026325dc6
Hélen Andrade <helenaraujoandrade@hotmail.com>
2026-05-11 19:55:22 -0300

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Header } from './components';
+
 import './App.css';
 
 const App = () => {
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header/>
+      <h1>Hello World</h1>
+    </>
   )
 };
 
-export default App;
+export { App };
