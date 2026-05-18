3f614fdef99faf1f4f4f01bef2331542a33210f4
Hélen Andrade <helenaraujoandrade@hotmail.com>
2026-05-17 21:55:08 -0300

    feat: add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { Header, Content, Footer } from './components';
-import { Home } from './pages';
-
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Home, AboutUs } from './pages';
+import { Home, AboutUs, NotFound } from './pages';
 import { DefaultLayout } from "./layouts";
 
 const Router = () => {
@@ -8,9 +8,9 @@ const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<DefaultLayout />}>
-            
                 <Route path="/" element={<Home />} /> 
                 <Route path="/about-us" element={<AboutUs />} />
+                <Route path="*" element={<NotFound />} /> 
             </Route>
         </Routes>
     );
